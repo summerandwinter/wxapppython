37fdf8eda34ece773d73ee015c838a23248c1053
winter <xiaopihailaotou@gmail.com>
2017-04-28 17:48:03 +0800

    增加新模板

--- images.py
+++ images.py
@@ -14,6 +14,7 @@ from leancloud.errors import LeanCloudError
 from PIL import Image, ImageColor, ImageFont, ImageDraw, ImageFilter
 from io import BytesIO
 from textwrap import *
+import re
 
 
 
@@ -118,6 +119,301 @@ def filter_unsharp_mask(request):
     fliter_data.close()
     return HttpResponse(msstream.getvalue(),content_type="image/jpeg")  
 
+def template(request):
+    w = 640
+    h = 862
+    iw = 600
+    ih = 340
+    title = '标题'
+    content = '觉得最失落的，大概是你还在为你们的未来出谋划策，他却已慢慢后退不再与你并肩。' 
+    spacing = 20
+    content = fill(content, 15)
+    author = '- 卫斯理 -' 
+    copyright = '微信小程序「时光砂砾」'  
+    title_fnt = ImageFont.truetype('font/zh/YueSong.ttf', 35)
+    content_fnt = ImageFont.truetype('font/zh/YueSong.ttf', 30)
+    author_fnt = ImageFont.truetype('font/zh/YueSong.ttf', 25)
+    copyright_fnt = ImageFont.truetype('font/zh/YueSong.ttf', 25)
+    base = Image.new('RGBA',(w,h),(255,255,255,255))
+    draw = ImageDraw.Draw(base)
+    tw,th = draw.multiline_textsize(title, font=title_fnt)
+    aw,ah = draw.multiline_textsize(author, font=author_fnt)
+    cw,ch = draw.multiline_textsize(content, font=content_fnt, spacing=spacing)
+    crw,crh = draw.multiline_textsize(copyright, font=copyright_fnt)
+    h = 635+th+ch+crh+ah;
+    base = Image.new('RGBA',(w,h),(255,255,255,255))
+    draw = ImageDraw.Draw(base)
+
+    
+
+    photo = Image.open("girl.jpg").convert('RGBA')
+
+    (pw, ph) = photo.size
+    if pw/ph>iw/ih:
+        box = ((pw-ph*iw/ih)/2,0,(pw+ph*iw/ih)/2,ph)
+    else:
+        box = (0,(ph-pw*ih/iw)/2,pw,(ph+pw*ih/iw)/2)  
+
+    photo = photo.crop(box)
+    photo = photo.resize((iw,ih))
+    base.paste(photo,box=(20,20))
+    # get a drawing context
+    draw = ImageDraw.Draw(base)
+    # draw text in the middle of the image, half opacity
+    draw.multiline_text((w/2-tw/2,420), title, font=title_fnt, fill=(0,0,0,255), align='center')
+    draw.multiline_text((w/2-cw/2,420+th+45), content, font=content_fnt, fill=(0,0,0,255), align='center', spacing=spacing)
+    draw.multiline_text((w/2-aw/2,420+th+45+ch+115), author, font=author_fnt, fill=(0,0,0,255), align='center')
+    draw.multiline_text((w-crw,420+th+45+ch+115+ah+50), copyright, font=copyright_fnt, fill=(189,189,189,255), align='center')
+   
+
+    # get BytesIO
+    msstream = BytesIO()
+    # save image data to output stream
+    base.save(msstream,"png")
+    # release memory
+    base.close()
+    return HttpResponse(msstream.getvalue(),content_type="image/png")  
+
+
+def template2(request):
+    w = 640
+    h = 1020
+    iw = 600
+    ih = 340
+    title = '标题'
+    content = '觉得最失落的，大概是你还在为你们的未来出谋划策，他却已慢慢后退不再与你并肩。' 
+    spacing = 20
+    padding = 2
+    author = '- 卫斯理 -' 
+    copyright = '微信小程序「时光砂砾」'  
+    title_fnt = ImageFont.truetype('font/zh/YueSong.ttf', 35)
+    content_fnt = ImageFont.truetype('font/zh/YueSong.ttf', 30)
+    author_fnt = ImageFont.truetype('font/zh/YueSong.ttf', 25)
+    copyright_fnt = ImageFont.truetype('font/zh/YueSong.ttf', 25)
+    base = Image.new('RGBA',(w,h),(255,255,255,255))
+    draw = ImageDraw.Draw(base)
+    aw,ah = draw.multiline_textsize(author, font=author_fnt)
+    crw,crh = draw.multiline_textsize(copyright, font=copyright_fnt)
+
+
+    
+
+    photo = Image.open("girl.jpg").convert('RGBA')
+
+    (pw, ph) = photo.size
+    if pw/ph>iw/ih:
+        box = ((pw-ph*iw/ih)/2,0,(pw+ph*iw/ih)/2,ph)
+    else:
+        box = (0,(ph-pw*ih/iw)/2,pw,(ph+pw*ih/iw)/2)  
+
+    photo = photo.crop(box)
+    photo = photo.resize((iw,ih))
+    base.paste(photo,box=(20,20))
+    # get a drawing context
+    draw = ImageDraw.Draw(base)
+    # split the title
+    tlines = wrap(title, 1)
+    # current title height
+    tnh = 420
+    # get width and height of single title word
+    stw,sth = title_fnt.getsize("已")
+    for tline in tlines:       
+        draw.text((w-115-stw,tnh), tline, fill=(0,0,0,255), font=title_fnt)
+        tnh = tnh+sth
+    # get width and height of single content word
+    scw,sch = content_fnt.getsize("已")    
+    clines = wrap(content, 14)
+    # current width of content
+    cnw = w-115-stw-115-scw
+    for cline in clines:
+        # current height of content 
+        cnh = 420 
+        cwords = wrap(cline, 1)
+        for cword in cwords:
+            pattern = re.compile("[，。、]+") 
+            if pattern.search(cword):
+                draw.text((cnw,cnh), cword, fill=(0,0,0,255), font=content_fnt)
+                # draw.text((cnw+30-12,cnh-30+12), cword, fill=(0,0,0,255), font=content_fnt)
+            else:
+                draw.text((cnw,cnh), cword, fill=(0,0,0,255), font=content_fnt)                           
+            cnh = cnh+sch+padding
+        cnw = cnw-scw-spacing   
+
+       
+    # draw text in the middle of the image, half opacity
+    # draw.multiline_text((w/2-tw/2,420), title, font=title_fnt, fill=(0,0,0,255), align='center')
+    # draw.multiline_text((w/2-cw/2,420+th+45), content, font=content_fnt, fill=(0,0,0,255), align='center', spacing=spacing)
+    draw.multiline_text((w/2-aw/2,h-50-15-crh-ah), author, font=author_fnt, fill=(0,0,0,255), align='center')
+    draw.multiline_text((w-crw,h-15-crh), copyright, font=copyright_fnt, fill=(189,189,189,255), align='center')
+   
+
+    # get BytesIO
+    msstream = BytesIO()
+    # save image data to output stream
+    base.save(msstream,"png")
+    # release memory
+    base.close()
+    return HttpResponse(msstream.getvalue(),content_type="image/png") 
+
+def template3(request):
+    w = 640
+    h = 862
+    iw = 600
+    ih = 340
+    bw = 300
+    bh = 300
+    title = '标题'
+    content = '觉得最失落的，大概是你还在为你们的未来出谋划策，他却已慢慢后退不再与你并肩。' 
+    spacing = 20
+    content = fill(content, 15)
+    author = '- 卫斯理 -' 
+    copyright = '微信小程序「时光砂砾」'  
+    title_fnt = ImageFont.truetype('font/zh/YueSong.ttf', 35)
+    content_fnt = ImageFont.truetype('font/zh/YueSong.ttf', 30)
+    author_fnt = ImageFont.truetype('font/zh/YueSong.ttf', 25)
+    copyright_fnt = ImageFont.truetype('font/zh/YueSong.ttf', 25)
+    base = Image.new('RGBA',(w,h),(255,255,255,255))
+    draw = ImageDraw.Draw(base)
+    tw,th = draw.multiline_textsize(title, font=title_fnt)
+    aw,ah = draw.multiline_textsize(author, font=author_fnt)
+    cw,ch = draw.multiline_textsize(content, font=content_fnt, spacing=spacing)
+    crw,crh = draw.multiline_textsize(copyright, font=copyright_fnt)
+    h = 695+th+ch+crh+ah;
+    base = Image.new('RGBA',(w,h),(255,255,255,255))
+    draw = ImageDraw.Draw(base)
+
+    
+
+    photo = Image.open("girl.jpg").convert('RGBA')
+
+    pw, ph = photo.size
+
+    if pw > ph:
+        box = ((pw-ph*bw/bh)/2,0,(pw+ph*bw/bh)/2,ph)
+    else:
+        box = (0,(ph-pw*bh/bw)/2,pw,(ph+pw*bh/bw)/2)  
+
+    photo = photo.crop(box)
+    photo = photo.resize((bw*4,bh*4))
+
+    circle = Image.new('L', (bw*4, bh*4), 0)
+    draw = ImageDraw.Draw(circle)
+    draw.ellipse((0, 0, bw*4, bh*4), fill=255)
+    alpha = Image.new('L', (bw*4, bh*4), 255)
+    alpha.paste(circle, (0, 0))
+    photo.putalpha(alpha)
+    photo = photo.resize((bw,bh),Image.ANTIALIAS)
+
+    base.paste(photo,box=(170,120),mask=photo)
+    # get a drawing context
+    draw = ImageDraw.Draw(base)
+    # draw text in the middle of the image, half opacity
+    draw.multiline_text((w/2-tw/2,480), title, font=title_fnt, fill=(0,0,0,255), align='center')
+    draw.multiline_text((w/2-cw/2,480+th+45), content, font=content_fnt, fill=(0,0,0,255), align='center', spacing=spacing)
+    draw.multiline_text((w/2-aw/2,480+th+45+ch+115), author, font=author_fnt, fill=(0,0,0,255), align='center')
+    draw.multiline_text((w-crw,480+th+45+ch+115+ah+50), copyright, font=copyright_fnt, fill=(189,189,189,255), align='center')
+   
+
+    # get BytesIO
+    msstream = BytesIO()
+    # save image data to output stream
+    base.save(msstream,"png")
+    # release memory
+    base.close()
+    return HttpResponse(msstream.getvalue(),content_type="image/png") 
+
+
+def template4(request):
+    w = 640
+    h = 1080
+    iw = 600
+    ih = 340
+    bw = 300
+    bh = 300
+    padding = 2
+    title = '标题'
+    content = '觉得最失落的，大概是你还在为你们的未来出谋划策，他却已慢慢后退不再与你并肩。' 
+    spacing = 20
+    content = fill(content, 15)
+    author = '- 卫斯理 -' 
+    copyright = '微信小程序「时光砂砾」'  
+    title_fnt = ImageFont.truetype('font/zh/YueSong.ttf', 35)
+    content_fnt = ImageFont.truetype('font/zh/YueSong.ttf', 30)
+    author_fnt = ImageFont.truetype('font/zh/YueSong.ttf', 25)
+    copyright_fnt = ImageFont.truetype('font/zh/YueSong.ttf', 25)
+    base = Image.new('RGBA',(w,h),(255,255,255,255))
+    draw = ImageDraw.Draw(base)
+    aw,ah = draw.multiline_textsize(author, font=author_fnt)
+    crw,crh = draw.multiline_textsize(copyright, font=copyright_fnt)
+
+    
+
+    photo = Image.open("girl.jpg").convert('RGBA')
+
+    pw, ph = photo.size
+
+    if pw > ph:
+        box = ((pw-ph*bw/bh)/2,0,(pw+ph*bw/bh)/2,ph)
+    else:
+        box = (0,(ph-pw*bh/bw)/2,pw,(ph+pw*bh/bw)/2)  
+
+    photo = photo.crop(box)
+    photo = photo.resize((bw*4,bh*4))
+
+    circle = Image.new('L', (bw*4, bh*4), 0)
+    draw = ImageDraw.Draw(circle)
+    draw.ellipse((0, 0, bw*4, bh*4), fill=255)
+    alpha = Image.new('L', (bw*4, bh*4), 255)
+    alpha.paste(circle, (0, 0))
+    photo.putalpha(alpha)
+    photo = photo.resize((bw,bh),Image.ANTIALIAS)
+
+    base.paste(photo,box=(170,120),mask=photo)
+    # get a drawing context
+    draw = ImageDraw.Draw(base)
+    # split the title
+    tlines = wrap(title, 1)
+    # current title height
+    tnh = 480
+    # get width and height of single title word
+    stw,sth = title_fnt.getsize("已")
+    for tline in tlines:       
+        draw.text((w-115-stw,tnh), tline, fill=(0,0,0,255), font=title_fnt)
+        tnh = tnh+sth
+    # get width and height of single content word
+    scw,sch = content_fnt.getsize("已")    
+    clines = wrap(content, 14)
+    # current width of content
+    cnw = w-115-stw-115-scw
+    for cline in clines:
+        # current height of content 
+        cnh = 480 
+        cwords = wrap(cline, 1)
+        for cword in cwords:
+            pattern = re.compile("[，。、]+") 
+            if pattern.search(cword):
+                draw.text((cnw,cnh), cword, fill=(0,0,0,255), font=content_fnt)
+                # draw.text((cnw+30-12,cnh-30+12), cword, fill=(0,0,0,255), font=content_fnt)
+            else:
+                draw.text((cnw,cnh), cword, fill=(0,0,0,255), font=content_fnt)                           
+            cnh = cnh+sch+padding
+        cnw = cnw-scw-spacing   
+
+       
+    # draw text in the middle of the image, half opacity
+    # draw.multiline_text((w/2-tw/2,420), title, font=title_fnt, fill=(0,0,0,255), align='center')
+    # draw.multiline_text((w/2-cw/2,420+th+45), content, font=content_fnt, fill=(0,0,0,255), align='center', spacing=spacing)
+    draw.multiline_text((w/2-aw/2,h-50-15-crh-ah), author, font=author_fnt, fill=(0,0,0,255), align='center')
+    draw.multiline_text((w-crw,h-15-crh), copyright, font=copyright_fnt, fill=(189,189,189,255), align='center')
+   
+
+    # get BytesIO
+    msstream = BytesIO()
+    # save image data to output stream
+    base.save(msstream,"png")
+    # release memory
+    base.close()
+    return HttpResponse(msstream.getvalue(),content_type="image/png") 
 def image_text(request): 
     fontSize = 40
     w = 640
